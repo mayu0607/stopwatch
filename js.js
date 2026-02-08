4cf73d037de00249c22834ee24ae126f53b859b7
mayu0607 <mayurisalunke06@gmail.com>
2026-02-08 21:22:40 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,72 @@
+var ms = 0, s = 0, m = 0, h = 0;
+var timer = null;
+
+var display = document.querySelector(".timer-Display");
+var laps = document.querySelector(".laps");
+
+function start() {
+    if (!timer) {
+        timer = setInterval(run, 10);
+    }
+}
+
+function run() {
+    ms++;
+    if (ms === 100) {
+        ms = 0;
+        s++;
+    }
+    if (s === 60) {
+        s = 0;
+        m++;
+    }
+    if (m === 60) {
+        m = 0;
+        h++;
+    }
+    display.innerHTML = getTimer();
+}
+
+function getTimer() {
+    return (
+        (h < 10 ? "0" + h : h) + " : " +
+        (m < 10 ? "0" + m : m) + " : " +
+        (s < 10 ? "0" + s : s) + " : " +
+        (ms < 10 ? "0" + ms : ms)
+    );
+}
+
+function pause() {
+    stopTimer();
+}
+
+function stopTimer() {
+    clearInterval(timer);
+    timer = null;
+}
+
+function reset() {
+    stopTimer();
+    ms = 0;
+    s = 0;
+    m = 0;
+    h = 0;
+    display.innerHTML = getTimer();
+}
+
+function restart() {
+    reset();
+    start();
+}
+
+function lap() {
+    if (timer) {
+        var li = document.createElement("li");
+        li.innerHTML = getTimer();
+        laps.appendChild(li);
+    }
+}
+
+function resetLap() {
+    laps.innerHTML = "";
+}
